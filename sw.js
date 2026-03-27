ae3ac4cb676adb592fd292f682f3b14e60b61456
sersamu <sersamu.2@gmail.com>
2026-03-27 09:51:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,29 @@
+const CACHE_NAME = 'cr-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar y guardar en caché
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Activar y limpiar versiones viejas
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    })
+  );
+});
+
+// Responder desde la caché si no hay internet
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
